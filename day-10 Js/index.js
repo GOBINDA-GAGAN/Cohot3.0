1bf2280d185ab90767b8d14521de3e1f1b18ef9d
Gobinda-Gagan <gobindagagandey@gmail.com>
2026-05-27 19:43:14 +0530

    first commit

---
+++ day-10 Js/index.js
@@ -0,0 +1,6 @@
+console.log("Hello world");
+console.error("this is Eror");
+console.warn("this is a warn");
+console.table("root")
+
+
---
+++ day-11 js/Phase-1_pratic.js
@@ -0,0 +1,53 @@
+console.log(`"Hello JavaScript"`);
+
+console.log({
+    name: "Gobinda Gagan Dey",
+    age: 21,
+    city: "Balasore"
+});
+
+console.error("this is error");
+
+let arr = [1, 2, 3, 45, 3];
+console.table(arr);
+
+let studentName = "Alok"
+let age = 21;
+console.log("age: ", age);
+let a = 10;
+let b = 20; //b=10 a=20
+console.log("a:", a, "b:", b);
+// let temp = a;
+// a = b;
+// b = temp;
+// a=a^b;
+// b=a^b;
+// a=a^b;
+console.log("a:", a, "b:", b);
+const PI = 3.14;
+console.log(PI);
+
+let A;
+console.log(A);
+let score = 20;
+console.log(score + 10);
+
+let firstname = "Gobinda Gagan "
+let lastName = "Dey"
+let fullName = firstname + lastName
+console.log(fullName);
+
+let name = " Gobinda"
+let age2 = 30;
+let islogIn = false;
+let user = null;
+let total;
+
+console.log(typeof name, typeof age, typeof islogIn ,typeof user, typeof total);
+
+let time=10000738274972954803903252890n
+console.log(typeof time);
+console.log(time);
+
+
+
