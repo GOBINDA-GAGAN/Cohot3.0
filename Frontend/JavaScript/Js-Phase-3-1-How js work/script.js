288bd990badcd07e9ba285b1ccb7e799b66e261a
Gobinda-Gagan <gobindagagandey@gmail.com>
2026-06-08 20:28:24 +0530

    Add syllabus PDF for DSA and initial JavaScript script for Phase 3

---
+++ Frontend/JavaScript/Js-Phase-3-1-How js work/script.js
@@ -0,0 +1,11 @@
+// what it js 
+//! single-thread sycronas  scripting language
+
+//GEC-Global Exectution Contex
+//Exetuatio Contex: a place where you js code will run
+//     |memory creation | code Execution   |
+//     |                |                   \
+//     |                |                   |
+//     |                |                   |
+//     |                |                   |
+//     |                |                   |
